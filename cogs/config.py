e988bc63c89f67a810243fab91f987e244112205
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2022-11-18 21:36:43 -0800

    started on creating phone gui with background img

--- cogs/config.py
+++ cogs/config.py
@@ -1,8 +1,26 @@
+from functools import partial
+from io import BytesIO
+
 import discord
 from discord import app_commands as ap
 from discord.ext import commands
+from PIL import Image
 
 from bot import PhoneBot
+
+
+def _pil_stuff(_bytes) -> discord.File:
+    image = Image.open(BytesIO(_bytes))
+    image = image.resize((750, 1334))
+
+    uiImage = Image.open("assets/default.png")
+    # Image.Image.paste(image, uiImage, (0, 0))
+
+    output_buffer = BytesIO()
+    image.save(output_buffer, "png")
+    output_buffer.seek(0)
+    file = discord.File(fp=output_buffer, filename="my_file.png")
+    return file
 
 
 class ConfigCog(commands.Cog):
@@ -19,8 +37,14 @@ class ConfigCog(commands.Cog):
     async def config_bg_image(
         self, inter: discord.Interaction, new_image: discord.Attachment
     ):
-        file = await new_image.to_file()
-        await inter.response.send_message(file=file)
+        if not "image" in str(new_image.content_type):
+            return await inter.response.send_message(
+                "You can only set images to your background image", ephemeral=True
+            )
+        _bytes = await new_image.read()
+        func = partial(_pil_stuff, _bytes)
+        img = await self.bot.loop.run_in_executor(None, func)
+        await inter.response.send_message(file=img)
 
 
 async def setup(bot: PhoneBot):
